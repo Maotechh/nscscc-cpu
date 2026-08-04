b6d2d0e737f92cf7ffdf008908951ff46ffbfaad
ECHO <yingbipao@gmail.com>
2026-08-04 22:51:44 +0800

    perf(l2): enable write-back policy

--- spinal/src/main/scala/openla500/core/OooCoreConfig.scala
+++ spinal/src/main/scala/openla500/core/OooCoreConfig.scala
@@ -87,7 +87,7 @@ final case class OooCoreConfig(
     enableDivideFastPath: Boolean = false,
     enableFastStoreCompletion: Boolean = true,
     enableStoreTranslationLookahead: Boolean = true,
-    enableL2WriteBack: Boolean = false,
+    enableL2WriteBack: Boolean = true,
     resetVector: BigInt = BigInt("1c000000", 16),
     instructionCache: OooCacheGeometry = OooCacheGeometry(ways = 2, sets = 64, lineBytes = 64),
     dataCache: OooCacheGeometry = OooCacheGeometry(ways = 2, sets = 64, lineBytes = 64),
